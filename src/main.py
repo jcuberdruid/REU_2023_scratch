404956f872c8aea5fae7332abdb8c529357ef84a
eos <eos@MAIN416-Mandy.uccs.edu>
2023-06-20 09:56:27 -0600

    working main.py for .npy generation

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import numpy as np
+import os
 import csv
 
 classesPath = "../data/datasets/classCSVs/"
 projectionPath = "../data/channelProjection.npy"
-outputDir = "../data/datasets/sequences"
+outputDir = "../data/datasets/sequences/"
 file = "MM_RLH_T1.csv"
-#csv_file = classesPath + file
+# csv_file = classesPath + file
 output = []
+
 
 def nicePrintNP(arr):
     padding = 2
@@ -39,53 +41,6 @@ def replace_indices_with_values(dictionary, array):
 
 
 # XXX add overlap here: (buffer of 10 added back to head of chunk list for 90-10 overlap)
-'''
-def processChunk(chunk):
-    # print(str(len(chunk)/80))
-    sequence_array = []
-    # len(chunk)/80 is 9.0125 <- 721/80 but 720/80 = 9 (0.2 seconds at beginning is baseline anyway)
-    chunk.pop(0)
-    while chunk:
-        print(f"size of chunk array {len(chunk)}")
-        frame_array = []
-        if len(chunk) == 80:
-            while chunk:
-                replace_indices_with_values(chunk.pop(), array)
-            sequence_array.append(frame_array)
-            break
-        for index, x in enumerate(chunk):
-            frame_array.append(replace_indices_with_values(chunk.pop(), array))
-            if len(frame_array) == 80:
-                sequence_array.append(frame_array)
-                nicePrintNPFloat(frame_array[0])
-                #print(f"length of frame_array: {len(frame_array)}")
-                #print(f"length of sequence_array: {len(sequence_array)}")
-                break
-        #if len(frame_array) != 80:
-            #print(f"while chunk exiting with frame_array at a length of {len(frame_array)}")
-    #print(len(sequence_array))
-    output.extend(sequence_array)
-   
-    #print(len(output))
-def processChunk(chunk):
-    sequence_array = []
-    chunk.pop(0)
-    while chunk:
-        frame_array = []
-        if len(chunk) == 80:
-            while chunk:
-                replace_indices_with_values(chunk.pop(), array)
-            sequence_array.append(frame_array)
-            break
-        index = 0  # Initialize index outside the loop
-        while index < len(chunk):
-            frame_array.append(replace_indices_with_values(chunk.pop(), array))
-            index += 1  # Increment the index manually
-            if len(frame_array) == 80:
-                sequence_array.append(frame_array)
-                break
-    output.extend(sequence_array)
-'''
 def processChunk(chunk):
     sequence_array = []
     chunk.pop(0)
@@ -94,7 +49,8 @@ def processChunk(chunk):
         if len(chunk) < 80:
             break
         for _ in range(80):
-            frame_array.append(replace_indices_with_values(chunk.pop(0), array))
+            frame_array.append(
+                replace_indices_with_values(chunk.pop(0), array))
         sequence_array.append(frame_array)
     output.extend(sequence_array)
 
@@ -122,26 +78,22 @@ def group_rows_preserve_order(rows, keys):
 
 
 array = np.load(projectionPath)
-count = 0
-def chunkEach(csv_file)
-	csv_file = classesPath + csv_file
-	with open(csv_file, 'r') as file:
-	    reader = csv.DictReader(file)
-	    rows = [row for row in reader]
 
-	    # Group rows based on specified keys while preserving order
-	    key_columns = ['subject', 'epoch', 'run']
-	    row_groups = group_rows_preserve_order(rows, key_columns)
 
-	    # Process each chunk
-	    for chunk in row_groups:
-		count = count + 1
-		processChunk(chunk)
+def chunkEach(csv_file):
+    csv_file = classesPath + csv_file
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        rows = [row for row in reader]
 
-print(str(count))
+        # Group rows based on specified keys while preserving order
+        key_columns = ['subject', 'epoch', 'run']
+        row_groups = group_rows_preserve_order(rows, key_columns)
 
-for x in output:
-	print(len(x))
+        # Process each chunk
+        for chunk in row_groups:
+            processChunk(chunk)
+
 
 def get_csv_files(directory):
     csv_files = []
@@ -150,10 +102,12 @@ def get_csv_files(directory):
             csv_files.append(filename)
     return csv_files
 
-# Example usage
+
 csv_files_list = get_csv_files(classesPath)
 
-print(csv_files_list)
-outputNp = np.array(output)
-
-np.save('my_array.npy', outputNp)
+for x in csv_files_list:
+    chunkEach(x)
+    outputNp = np.array(output)
+    x = x.split(".")[0]
+    np.save(outputDir+x+".npy", outputNp)
+    output.clear()
