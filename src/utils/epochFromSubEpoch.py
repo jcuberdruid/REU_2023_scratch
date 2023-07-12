c991ed3dcc4d9e8cd7678c512e474c234baf1319
eos <eos@uhal134c-lap1.uccs.edu>
2023-07-12 10:16:40 -0600

    sigh

--- src/utils/epochFromSubEpoch.py
+++ src/utils/epochFromSubEpoch.py
@@ -16,6 +16,19 @@ output: dataEpochs.npy(n/15, 1200, 17, 17), annotationsEpochs.npy
 '''
 #load data
 #load annotation
+path = "../../data/datasets/processed4/sequences/MM_RLH_T1.npy"
+pathannotations = "../../data/datasets/processed4/sequences/MM_RLH_T1_annotations.csv"
 
 
+#load annotation
+annotations = []
+with open(pathannotations, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            annotations_arr.append(row)
+    annotations.append(annotations_arr)
 
+print(annotations[0])
+#load data
+npyLoad = np.load(path)
+print(npyLoad.shape)
